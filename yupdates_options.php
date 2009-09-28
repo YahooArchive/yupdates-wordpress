af22dad1e34ce1f540d6da1dbcb5396f3601b4d8
basictheory <zagraves@gmail.com>
2009-09-28 11:23:07 -0700

    new setup flow from micah

--- yupdates_options.php
+++ yupdates_options.php
@@ -9,7 +9,7 @@
  * @package    yos-updates-wordpress
  * @subpackage yahoo
  *
- * @author     Ryan Kennedy <rckenned@yahoo-inc.com>, Lawrence Morrisroe <lem@yahoo-inc.com>, Zach Graves <zachg@yahoo-inc.com>
+ * @author     Ryan Kennedy, Lawrence Morrisroe <lem@yahoo-inc.com>, Zach Graves <zachg@yahoo-inc.com>
  * @copyright  Copyrights for code authored by Yahoo! Inc. is licensed under the following terms:
  * @license    BSD Open Source License
  *
@@ -48,33 +48,22 @@ function yupdates_plugin_options() {
 		font-weight: bold;
 	}
 </style>
+<script type="text/javascript">
+<!--
+function switchMenu(obj) {
+	var el = document.getElementById(obj);
+	if ( el.style.display != "none" ) {
+		el.style.display = 'none';
+	}
+	else {
+		el.style.display = '';
+	}
+}
+//-->
+</script>
 <div class="wrap">
-	<h3 class="authTitle">Get a Yahoo! API Key</h3>
-	<p>1. Go to the <a href="https://developer.apps.yahoo.com/dashboard/createKey.html" target="_new">
-		Yahoo! Developer Network</a> to register for an API key and complete 
-		the form with the following information:</p>
-	<p><img src="<?php echo get_option('siteurl'); ?>/wp-content/plugins/yupdates-wordpress/images/auth_step1_ydn.png" width="632" height="220"></p>
-	
-	<p class="authStep"><strong>Select access to private user data.</strong></p>
-	<p><img src="<?php echo get_option('siteurl'); ?>/wp-content/plugins/yupdates-wordpress/images/auth_step2_scopes.png"></p>
-
-	<p class="authStep"><strong>3. Select Read/Write access to Yahoo! Updates</strong></td>
-	<p><img src="<?php echo get_option('siteurl'); ?>/wp-content/plugins/yupdates-wordpress/images/auth_step3_read-write.png"></p>
-
-	<p class="authStep"><strong>4. Agree to the Yahoo! Terms of Use and click Get API Key.</strong></p>
-	<p><img src="<?php echo get_option('siteurl'); ?>/wp-content/plugins/yupdates-wordpress/images/auth_step4_apikey.png"></p>
-
-	<p class="authStep"><strong>5. If you haven't already done so, verify 
-		ownership of your domain to Yahoo! by creating a file of a specific 
-		name and uploading that to the root directory of your domain.</strong></p>
-	<p><img src="<?php echo get_option('siteurl'); ?>/wp-content/plugins/yupdates-wordpress/images/auth_step5_domain.png"></p>
-	
-	<p class="authStep"><strong>6. Once you've successfully created your API 
-		key, copy your API key information to the Plugin Settings below:</strong></p>
-	<p><img src="<?php echo get_option('siteurl'); ?>/wp-content/plugins/yupdates-wordpress/images/auth_step6_success.png" width="743" height="413"></p>
 	
 	<a name="settings"></a>
-	<h3 class="authTitle">Yahoo! Plugin Settings</h3>
 	
     <form method="post" action="options.php">
 	<?php 
@@ -82,6 +71,9 @@ function yupdates_plugin_options() {
             wp_nonce_field('update-options'); 
         }
 	?>
+	
+	<h3 class="authTitle">Yahoo! API Access Settings</h3>
+Enter your API Key, Shared Secret, and App ID from the Yahoo! Developer Network. (These are needed so your WordPress blog can read and write data on your behalf without revealing your Yahoo! ID and password). 
         <table class="form-table">
             <tr valign="top">
                 <th scope="row">Yahoo! API Key</th>
@@ -95,10 +87,58 @@ function yupdates_plugin_options() {
                 <th scope="row">Yahoo! Application ID</th>
                 <td><input type="text" size="20" name="yupdates_application_id" value="<?php echo $appid; ?>" /></td>
             </tr>
+	</table>
+
+<p><em>Don't know what these are, or how to get them?</em> <a onclick="switchMenu('ydnhelp');" title="Switch the Menu">Show/hide instructions for how to get a Yahoo! API key.</a> (It's quick and free.)</p>
+
+<div id="ydnhelp" style="display:none; border: 1px solid #cccccc; margin: 10px; padding: 20px;">
+
+	<h4 class="authTitle">How to get a Yahoo! API Key</h4>
+	<ol>
+	<li><p>Go to the <a href="https://developer.apps.yahoo.com/dashboard/createKey.html" target="_new">
+		Yahoo! Developer Network</a> to register for an API key, and complete 
+		the form with the following information:</p>
+		<ol>
+			<li type="a"><strong>Application Name:</strong> Enter "<?php bloginfo('name'); ?>"</li>
+			<li type="a"><strong>Kind of Application:</strong> Choose "Web-based"</li>
+			<li type="a"><strong>Description:</strong> Describe your site here.</li>
+			<li type="a"><strong>Application URL:</strong> Enter "<?php bloginfo('url'); ?>"</li>
+			<li type="a"><strong>Favicon URL:</strong> Provide the URL to a GIF, JPG or PNG image (note: ICO is <em>not</em> supported) to serve as the favicon for your blog, if you have one. This will be used if you generate a Yahoo! Update with each blog post.<br />(e.g. <img src="<?php echo get_option('siteurl'); ?>/wp-content/plugins/yupdates-wordpress/images/favicon.png" width="16" height="16" align="bottom" /> John posted "My First iPhone Experiences" from his blog: Mac User Fans. )</li>
+			<li type="a"><strong>Application Domain:</strong> Enter "<?php echo sprintf("http://%s", $_SERVER['HTTP_HOST']) ?>"</li>
+		</ol>
+	 </li>
+	
+	<li><p class="authStep"><strong>Choose access to private user data.</strong></p>
+	<p><img src="<?php echo get_option('siteurl'); ?>/wp-content/plugins/yupdates-wordpress/images/auth_step2_scopes.png"></p></li>
+
+	<li><p class="authStep"><strong>Choose Read/Write access to Yahoo! Updates.</strong></td>
+	<p><img src="<?php echo get_option('siteurl'); ?>/wp-content/plugins/yupdates-wordpress/images/auth_step3_read-write.png"></p></li>
+
+	<li><p class="authStep"><strong>Agree to the Yahoo! Terms of Use, and click the "Get API Key" button.</strong></p>
+	<p><img src="<?php echo get_option('siteurl'); ?>/wp-content/plugins/yupdates-wordpress/images/auth_step4_apikey.png"></p></li>
+
+	<li><p class="authStep"><strong>If you haven't done so previously, verify 
+		ownership of your domain with Yahoo! by creating a file of a specific 
+		name and uploading that to the root directory of your domain.</strong></p></li>
+	
+	<li><p class="authStep"><strong>Once you've successfully created your API 
+		key, copy your API key information from the success screen (sample below) to the Yahoo! API Access Settings below:</strong></p>
+	<p><img src="<?php echo get_option('siteurl'); ?>/wp-content/plugins/yupdates-wordpress/images/auth_step6_success.png" width="562" height="233"></p></li>
+	
+	</ol>
+</div>
+
+
+<hr noshade="noshade" />
+	<h3 class="authTitle">Yahoo! Updates Settings</h3>
+	    <table class="form-table">
             <tr valign="top">
-				<th scope="row">Customize your Yahoo! Updates stream:</th>
+				<th scope="row">Customize your Yahoo! Updates event display:</th>
 				<td>
-					<p>&lt;Your Yahoo! name&gt;<input type="text" size="50" name="yupdates_title_template" value="<?php echo $title_template; ?>" /></p>
+					<p>&lt;Your Yahoo! name&gt;<input type="text" size="50" name="yupdates_title_template" value="<?php echo $title_template; ?>" /><br /><small>Use the following tags in the display field above:</small><br /><ul>
+<li><small>"#blog_title" = the title of your blog post</small></li>
+<li><small>"#blog_name" = the name of your blog (i.e. "<?php bloginfo('name'); ?>")</small></li>
+</ul></p>
 				</td>
 			</tr>
         </table>
